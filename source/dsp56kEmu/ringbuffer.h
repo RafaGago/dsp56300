e34960af9631d058945320ba676b7b38fa07ce42
Rafael Gago <rafael.gago@zenseact.com>
2021-10-16 12:32:17 +0200

    semaphore: Try to minimize syscalls
    This commit adds an atomic variable to try to avoid syscalls on the
    semaphore, so when there are "tickets" available no function with the
    potential to make a syscall is called. It aims specially to avoid
    unneeded calls to notify, as a non contended mutex is usally solved in
    user-space too.
    
    This check will be slightly detrimental when there are waiters, as
    another CAS operation is added on top.
    
    From the practical point of view, on this codebase it is not affecting
    performance but it might help preventing spikes.
    
    Redesigning ringbuffer seems possible but not to be a problem at
    present. Maybe that queue can be traced on the future to see if it
    sometimes causes spikes.

--- source/dsp56kEmu/ringbuffer.h
+++ source/dsp56kEmu/ringbuffer.h
@@ -11,31 +11,32 @@
 
 namespace dsp56k
 {
-	template<typename T, size_t C, bool Lock> class RingBuffer
+	template <typename T, size_t C, bool Lock> class RingBuffer
 	{
 	public:
 		RingBuffer() : m_insertPos(0), m_removePos(0), m_usage(0), m_readSem(0), m_writeSem(static_cast<int>(C))
 		{
-			static_assert(C>0, "C needs to be greater than 1");
-			static_assert((C&(C-1)) == 0, "C needs to be power of two");
+			static_assert(C > 0, "C needs to be greater than 1");
+			static_assert((C & (C - 1)) == 0, "C needs to be power of two");
 		}
 
-		size_t capacity() const		{ return C; }
-		bool empty() const			{ return m_usage == 0; }
-		bool full() const			{ return m_usage == C; }
-		size_t size() const			{ return m_usage; }
-		size_t remaining() const	{ return (C - m_usage); }
+		size_t capacity() const { return C; }
+		bool empty() const { return m_usage == 0; }
+		bool full() const { return m_usage == C; }
+		size_t size() const { return m_usage; }
+		size_t remaining() const { return (C - m_usage); }
 
-		void push_back( const T& _val )
+		void push_back(const T &_val)
 		{
-	//		assert( m_usage < C && "ring buffer is already full!" );
+			//		assert( m_usage < C && "ring buffer is already full!" );
 
 			m_writeSem.wait();
 
 			m_data[m_insertPos] = _val;
 			updateCounter(m_insertPos);
 
-			// usage need to be incremented AFTER data has been written, otherwise, reader thread would read incomplete data
+			// usage need to be incremented AFTER data has been written, otherwise, reader thread would read incomplete
+			// data
 			++m_usage;
 
 			m_readSem.notify();
@@ -46,7 +47,7 @@ namespace dsp56k
 			m_readSem.wait();
 
 			T res = front();
-	//		assert( m_usage > 0 && "ring buffer is already empty!" );
+			//		assert( m_usage > 0 && "ring buffer is already empty!" );
 
 			updateCounter(m_removePos);
 
@@ -57,9 +58,9 @@ namespace dsp56k
 			return res;
 		}
 
-		void removeAt( size_t i )
+		void removeAt(size_t i)
 		{
-			if( !i )
+			if (!i)
 			{
 				pop_front();
 				return;
@@ -67,131 +68,119 @@ namespace dsp56k
 
 			convertIdx(i);
 
-			std::swap( m_data[i], m_data[m_removePos] );
+			std::swap(m_data[i], m_data[m_removePos]);
 
 			pop_front();
 		}
 
-		T& operator[](size_t i)
-		{
-			return get(i);
-		}
+		T &operator[](size_t i) { return get(i); }
 
-		const T& operator[](size_t i) const
-		{
-			return const_cast< RingBuffer<T,C, Lock>* >(this)->get(i);
-		}
+		const T &operator[](size_t i) const { return const_cast<RingBuffer<T, C, Lock> *>(this)->get(i); }
 
-		const T& front() const
-		{
-			return m_data[m_removePos];
-		}
-		
-		T& front()
-		{
-			return m_data[m_removePos];
-		}
+		const T &front() const { return m_data[m_removePos]; }
+
+		T &front() { return m_data[m_removePos]; }
 
 		void clear()
 		{
-			while( !empty() )
+			while (!empty())
 				pop_front();
 		}
 
 		void waitNotEmpty() const
 		{
-			if constexpr(Lock)
+			if constexpr (Lock)
 				return;
-			while(empty())
+			while (empty())
 				std::this_thread::yield();
 		}
 
 		void waitNotFull() const
 		{
-			if constexpr(Lock)
+			if constexpr (Lock)
 				return;
-			while(full())
+			while (full())
 				std::this_thread::yield();
 		}
 
 	private:
-		static void updateCounter( size_t& _counter )
+		static void updateCounter(size_t &_counter)
 		{
 			++_counter;
-			_counter &= C-1;
+			_counter &= C - 1;
 		}
 
-		T& get( size_t i )
+		T &get(size_t i)
 		{
-			convertIdx( i );
+			convertIdx(i);
 
 			return m_data[i];
 		}
 
-		void convertIdx( size_t& _i ) const
+		void convertIdx(size_t &_i) const
 		{
 			_i += m_removePos;
 
-			_i &= C-1;
+			_i &= C - 1;
 		}
 
-		std::array<T,C>		m_data;
+		std::array<T, C> m_data;
 
-		size_t				m_insertPos;
-		size_t				m_removePos;
-		std::atomic<size_t>	m_usage;
+		size_t m_insertPos;
+		size_t m_removePos;
+		std::atomic<size_t> m_usage;
 
-		typedef typename std::conditional<Lock, Semaphore, NopSemaphore>::type Sem;
+		using Sem = std::conditional_t<Lock, Semaphore, NopSemaphore>;
 
-		Sem					m_readSem;
-		Sem					m_writeSem;
+		Sem m_readSem;
+		Sem m_writeSem;
 
 	public:
 		static void test()
 		{
-			RingBuffer<int,10, false> rb;
+			RingBuffer<int, 10, false> rb;
 
-			assert( rb.size() == 0 );
-			assert( rb.empty() );
-			assert( rb.remaining() == 10 );
+			assert(rb.size() == 0);
+			assert(rb.empty());
+			assert(rb.remaining() == 10);
 
-			rb.push_back( 3 );
-			rb.push_back( 4 );
-			rb.push_back( 5 );
-			rb.push_back( 6 );
+			rb.push_back(3);
+			rb.push_back(4);
+			rb.push_back(5);
+			rb.push_back(6);
 
-			assert( rb.size() == 4 );
-			assert( !rb.empty() );
-			assert( rb.remaining() == 6 );
+			assert(rb.size() == 4);
+			assert(!rb.empty());
+			assert(rb.remaining() == 6);
 
-			assert( rb[2] == 5 );
-
-			rb.pop_front();
-
-			assert( rb.size() == 3 );
-			assert( !rb.empty() );
-			assert( rb.remaining() == 7 );
-
-			assert( rb[2] == 6 );
-			assert( rb[0] == 4 );
-			assert( rb.front() == 4 );
+			assert(rb[2] == 5);
 
 			rb.pop_front();
+
+			assert(rb.size() == 3);
+			assert(!rb.empty());
+			assert(rb.remaining() == 7);
+
+			assert(rb[2] == 6);
+			assert(rb[0] == 4);
+			assert(rb.front() == 4);
+
+			rb.pop_front();
 			rb.pop_front();
 			rb.pop_front();
 
-			assert( rb.size() == 0 );
-			assert( rb.empty() );
-			assert( rb.remaining() == 10 );
+			assert(rb.size() == 0);
+			assert(rb.empty());
+			assert(rb.remaining() == 10);
 
 			rb.push_back(77);
 
-			assert( rb.size() == 1 );
-			assert( !rb.empty() );
-			assert( rb.remaining() == 9 );
+			assert(rb.size() == 1);
+			assert(!rb.empty());
+			assert(rb.remaining() == 9);
 
-			assert( rb.front() == 77 );
-			assert( rb[0] == 77 );
+			assert(rb.front() == 77);
+			assert(rb[0] == 77);
 		}
 	};
-}
+} // namespace dsp56k
--- source/dsp56kEmu/semaphore.h
+++ source/dsp56kEmu/semaphore.h
@@ -1,49 +1,84 @@
 #pragma once
 
-#include <mutex>
+#include <atomic>
 #include <condition_variable>
+#include <mutex>
 
 namespace dsp56k
 {
+	// An unfair (first consumer blocking may not be the first notified)
+	// user-space semaphore.
+	class BlockingSemaphore
+	{
+	public:
+		explicit BlockingSemaphore(const int _count = 0) : m_count(_count) {}
+
+		void notify()
+		{
+			Lock lock(m_mutex);
+			m_count++;
+			// notify the waiting thread
+			m_cv.notify_one();
+		}
+
+		void wait()
+		{
+			Lock lock(m_mutex);
+
+			while (m_count == 0)
+			{
+				// wait on the mutex until notify is called
+				m_cv.wait(lock);
+			}
+
+			m_count--;
+		}
+
+	private:
+		using Lock = std::unique_lock<std::mutex>;
+		std::mutex m_mutex;
+		std::condition_variable m_cv;
+		int m_count;
+	};
+
+	// An unfair (first consumer blocking may not be the first notified)
+	// user-space semaphore that avoids blocking when there are available
+	// notifications.
 	class Semaphore
 	{
 	public:
-		explicit Semaphore (const int _count = 0) : m_count(_count) 
-	    {
-	    }
-	    
-	    void notify()
-		{
-	        Lock lock(m_mutex);
-	        m_count++;
-	        //notify the waiting thread
-	        m_cv.notify_one();
-	    }
+		explicit Semaphore(const int _count = 0) : m_sem(0), m_gate(_count) {}
 
-	    void wait()
+		void notify()
 		{
-	        Lock lock(m_mutex);
-
-	    	while(m_count == 0)
+			int prev = m_gate.fetch_add(1, std::memory_order_release);
+			if (prev >= 0)
 			{
-	            // wait on the mutex until notify is called
-	            m_cv.wait(lock);
-	        }
+				return;
+			}
+			m_sem.notify();
+		}
 
-	    	m_count--;
-	    }
+		void wait()
+		{
+			int prev = m_gate.fetch_sub(1, std::memory_order_acquire);
+			if (prev > 0)
+			{
+				return;
+			}
+			m_sem.wait();
+		}
+
 	private:
-		using Lock = std::unique_lock<std::mutex>;
-	    std::mutex m_mutex;
-	    std::condition_variable m_cv;
-	    int m_count;
+		std::atomic<int> m_gate;
+		BlockingSemaphore m_sem;
 	};
 
 	class NopSemaphore
 	{
 	public:
-		explicit NopSemaphore (const int _count = 0) {}
-	    void notify()	    {}
-		void wait()			{}
+		explicit NopSemaphore(const int _count = 0) {}
+		void notify() {}
+		void wait() {}
 	};
-};
+}; // namespace dsp56k
